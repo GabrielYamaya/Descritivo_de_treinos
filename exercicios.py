db47d4d3df10f51f5e7112de5040143e7446a3f9
Gabriel Yamaya <yamaya.gabriel@gmail.com>
2023-08-27 13:48:31 -0300

    Atualização exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,15 @@
+#%% [marckdown]
+# # Exercício de operadores lógicos
+
+#%%
+trabalho_terca = True
+trabalho_quarta = False
+
+tv50 = trabalho_terca and trabalho_quarta
+tv32 = trabalho_terca != trabalho_quarta
+sorvete = trabalho_terca or trabalho_quarta
+saude = not sorvete
+
+print("Tv50 = {}, Tv32 = {}, Sorvete = {}, Saúde = {}".format(tv50, tv32, sorvete, saude))
+print("Tv50 = {1}, Tv32 = {0}".format(tv32, tv50)) #da para trabalhar com índice também
+# %%
